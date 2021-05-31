2c33ba97f505379f8654ea486916bd46c662fbe7
anandvaibhav22 <63141259+anandvaibhav22@users.noreply.github.com>
2021-05-31 23:34:18 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+'''
 import sys
 
 def add(a,b):
@@ -5,3 +6,16 @@ def add(a,b):
     
 if __name__ == "__main__":
 	add(1,2)
+'''
+import sys
+
+def add (a,b):
+    return int(a)+int(b)
+
+def main():
+    input_var = sys.argv[1]
+    input_var1 = sys.argv[2]
+    print(add(input_var,input_var1))
+
+if __name__ == '__main__':
+    main()
